c4481903982447c7f3514f2769fdede89019dca3
Deepak <deepakchoudhary88088@gmail.com>
2025-12-18 01:09:26 +0530

    remove student

--- src/pages/student/index.ts
+++
@@ -1,7 +0,0 @@
-// Student Portal Pages
-export { default as Dashboard } from './Dashboard';
-export { default as Applications } from './Applications';
-export { default as Compare } from './Compare';
-export { default as Documents } from './Documents';
-export { default as Notifications } from './Notifications';
-export { default as Profile } from './Profile';
